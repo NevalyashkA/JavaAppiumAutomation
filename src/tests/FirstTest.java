dcdd56ad855839437db4fbd4ebc28667d4288f0c
Daria Shniydmiller <DVDyachenko@sberbank.ru>
2022-03-28 12:57:37 +0700

    Ex7*: Поворот экрана

--- src/tests/FirstTest.java
+++ src/tests/FirstTest.java
@@ -38,6 +38,7 @@ public class FirstTest {
         capabilities.setCapability("app","/Users/u18285002/IdeaProjects/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
     @After
