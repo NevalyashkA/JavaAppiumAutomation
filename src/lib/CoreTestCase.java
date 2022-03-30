ad906d7c940374d4e30563e309a94814be1c607b
Daria Shniydmiller <DVDyachenko@sberbank.ru>
2022-03-30 15:06:45 +0700

    01. CoreTestCase

---
+++ src/lib/CoreTestCase.java
@@ -0,0 +1,37 @@
+package lib;
+
+import junit.framework.TestCase;
+
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class CoreTestCase extends TestCase {
+    protected AppiumDriver driver;
+    private static String AppiumUrl = "http://127.0.0.1:4723/wd/hub";
+
+    @Override
+    public void setUp() throws Exception{
+        super.setUp();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/u18285002/IdeaProjects/JavaAppiumAutomation/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL(AppiumUrl),capabilities);
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    @Override
+    public void tearDown () throws Exception{
+        driver.quit();
+        super.tearDown();
+    }
+
+}
